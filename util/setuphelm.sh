36bc4227a7932550fb9807590e6eb26fe4807bd6
Michael R. Hines <michael@hinespot.com>
2019-05-23 02:19:29 -0500

    new changes

--- util/setuphelm.sh
+++ util/setuphelm.sh
@@ -493,7 +493,7 @@ echo "Registry located at: ${PRIVATE_REGISTRY}"
 
 NODES=$(kubectl get nodes | grep Ready | wc -l)
 SSHKEY="$(cat ~/.ssh/id_rsa.pub)"
-PRIVKEY="$(cat ~/.ssh/id_rsa)"
+PRIVKEY="$(cat ~/.ssh/id_rsa | sed ':a;N;$!ba;s/\n/\\\\n/g')"
 
 echo "Performing backdoor reconfiguration on ${NODES} nodes... "
 
@@ -532,7 +532,7 @@ spec:
       - name: entrypoint
         image: ubuntu:18.04
         command: ["/bin/bash"]
-        args: ["-c", "/bin/echo \$PRIVKEY > id_rsa && /bin/chmod go-rx id_rsa && /usr/bin/apt update && /usr/bin/apt install -y iproute2 ssh && dest=\$(ip route | grep default | cut -d ' ' -f 3) && /bin/echo \$SSHKEY >> /tmp/slash/root/.ssh/authorized_keys && /bin/echo '{ \"insecure-registries\" : [\"${PRIVATE_REGISTRY}\"] }' > /tmp/slash/etc/docker/daemon.json && sed -ie 's/\\\\:0\\\\:0/\\\\:99999\\\\:0/g' /tmp/slash/etc/shadow && /usr/bin/ssh root@\$dest \"service docker restart\" && /bin/sleep infinity"]
+        args: ["-c", "/bin/echo -e \"\$PRIVKEY\" > id_rsa; /bin/chmod go-rx id_rsa; /usr/bin/apt update; /usr/bin/apt install -y iproute2 ssh; dest=\$(ip route | grep default | cut -d ' ' -f 3); /bin/echo \$SSHKEY >> /tmp/slash/root/.ssh/authorized_keys; /bin/echo '{ \"insecure-registries\" : [\"${PRIVATE_REGISTRY}\"] }' > /tmp/slash/etc/docker/daemon.json; sed -ie 's/\\\\:0\\\\:0/\\\\:99999\\\\:0/g' /tmp/slash/etc/shadow; /usr/bin/ssh -i id_rsa -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no root@\$dest \"service docker restart\"; /bin/sleep infinity"]
         env:
         - name: SSHKEY
           value: "${SSHKEY}"
